fa5806b682fc6b0d74e2d6539d4887219a5c46da
varun <varunprashanthnaik@gmail.com>
2024-08-16 14:27:34 +0530

    search by name added

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,12 +1,31 @@
 import express from "express";
-import { Register, Login, getUser } from "../controllers/user.js";
+import { Register, Login, getUser} from "../controllers/user.js";
 import { verifyJwtToken } from "../middleware/verify.js";
 import upload from "../middleware/upload.js";
+import User from "../models/User.js";
 
 const router = express.Router();
 
 router.post("/register", upload.single("profile_pic"), Register);
 router.post("/login", Login);
 router.get("/:userId", getUser);
+
+
+router.get('/getbyname/:username', async (req, res) => {
+    console.log('Request Params:', req.params);
+    const username = req.params.username;
+    try {
+      const user = await User.findOne({ where: { username } });
+      console.log('User Found:', user);
+      if (user) {
+        res.json({ userId: user.id });
+      } else {
+        res.status(404).json({ message: 'User not found' });
+      }
+    } catch (error) {
+      console.error('Error fetching user:', error);
+      res.status(500).json({ error: error.message });
+    }
+  });
 
 export default router;
